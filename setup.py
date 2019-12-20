47d870f2a326efd6a5f1b418d8a4128a20335181
Varuna Jayasiri <vpjayasiri@gmail.com>
2019-12-20 09:24:47 +0530

    📇 setup.py

---
+++ setup.py
@@ -0,0 +1,31 @@
+import setuptools
+
+with open("README.md", "r") as f:
+    long_description = f.read()
+
+print(setuptools.find_packages())
+
+setuptools.setup(
+    name='lab',
+    version='3.0',
+    author="Varuna Jayasiri",
+    author_email="vpjayasiri@gmail.com",
+    description="🧪 Organize Machine Learning Experiments",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/vpj/lab",
+    packages=setuptools.find_packages(exclude=('samples', 'samples.*')),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        'Intended Audience :: Developers',
+        'Intended Audience :: Science/Research',
+        'Topic :: Scientific/Engineering',
+        'Topic :: Scientific/Engineering :: Mathematics',
+        'Topic :: Scientific/Engineering :: Artificial Intelligence',
+        'Topic :: Software Development',
+        'Topic :: Software Development :: Libraries',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+    keywords='machine learning',
+)
