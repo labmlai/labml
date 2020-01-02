8c6d4947d209cf54583e7007bbf108011977a275
Varuna Jayasiri <vpjayasiri@gmail.com>
2020-01-02 11:49:08 +0530

    ✨  screen writer decimal places auto adjust

--- lab/logger/writers/__init__.py
+++ lab/logger/writers/__init__.py
@@ -19,6 +19,39 @@ class ScreenWriter(Writer):
         super().__init__()
 
         self.is_color = is_color
+        self._estimates = {}
+        self._beta = 0.9
+        self._beta_pow = {}
+
+    def update_estimate(self, k, v):
+        if k not in self._estimates:
+            self._estimates[k] = 0
+            self._beta_pow[k] = 1.
+
+        self._estimates[k] *= self._beta
+        self._estimates[k] += (1 - self._beta) * v
+        self._beta_pow[k] *= self._beta
+
+    def get_empty_string(self, length, decimals):
+        return ' ' * (length - 2 - decimals) + '-.' + '-' * decimals
+
+    def get_value_string(self, k, v):
+        if k not in self._estimates:
+            assert v is None
+            return self.get_empty_string(8, 2)
+
+        estimate = self._estimates[k] / (1 - self._beta_pow[k])
+        lg = int(np.floor(np.log10(estimate))) + 1
+
+        decimals = 7 - lg
+        decimals = max(1, decimals)
+        decimals = min(6, decimals)
+
+        fmt = "{v:8." + str(decimals) + "f}"
+        if v is None:
+            return self.get_empty_string(8, decimals)
+        else:
+            return fmt.format(v=v)
 
     def write(self, *,
               global_step: int,
@@ -30,15 +63,18 @@ class ScreenWriter(Writer):
             if not ind.options.is_print:
                 continue
 
-            if len(values[k]) == 0:
-                continue
-
-            v = np.mean(values[k])
-
             parts.append((f" {k}: ", None))
-            if self.is_color:
-                parts.append((f"{v :8,.2f}", Text.value))
+
+            if len(values[k]) == 0:
+                value = self.get_value_string(k, None)
             else:
-                parts.append((f"{v :8,.2f}", None))
+                v = np.mean(values[k])
+                self.update_estimate(k, v)
+                value = self.get_value_string(k, v)
+
+            if self.is_color:
+                parts.append((value, Text.value))
+            else:
+                parts.append(value)
 
         return parts
