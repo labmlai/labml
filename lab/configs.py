d1bf24c3b9e16a03075dffed40091c8ebfbbb18e
Varuna Jayasiri <vpjayasiri@gmail.com>
2020-05-08 09:42:47 +0530

    configs docs

--- lab/configs.py
+++ lab/configs.py
@@ -2,4 +2,7 @@ from lab.internal.configs import Configs as _Configs
 
 
 class BaseConfigs(_Configs):
+    r"""
+    You should sub-class this class to create your own configurations
+    """
     pass
