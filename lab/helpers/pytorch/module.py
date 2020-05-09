77fd7b57cf8f0edfc47cdd921da2f17479bf288e
Varuna Jayasiri <vpjayasiri@gmail.com>
2020-05-09 08:02:21 +0530

    module

---
+++ lab/helpers/pytorch/module.py
@@ -0,0 +1,30 @@
+import torch.nn
+
+
+class Module(torch.nn.Module):
+    r"""
+    Wraps ``torch.nn.Module`` to overload ``__call__`` instead of
+    ``forward`` for better type checking.
+    """
+
+    def __init_subclass__(cls, **kwargs):
+        if cls.__dict__.get('__call__', None) is None:
+            return
+
+        setattr(cls, 'forward', cls.__dict__['__call__'])
+        delattr(cls, '__call__')
+
+    @property
+    def device(self):
+        params = self.parameters()
+        try:
+            sample_param = next(params)
+            return sample_param.device
+        except StopIteration:
+            raise RuntimeError(f"Unable to determine"
+                               f" device of {self.__class__.__name__}") from None
+
+
+if __name__ == '__main__':
+    m = Module()
+    print(m.device)
