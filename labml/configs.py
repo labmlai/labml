67ef2a6f225718667d8c33c6a6abb7dc7d69b035
Varuna Jayasiri <vpjayasiri@gmail.com>
2020-05-13 15:36:04 +0530

    option Configs.attribute

--- labml/configs.py
+++ labml/configs.py
@@ -1,4 +1,7 @@
+from typing import Callable
+
 from labml.internal.configs.base import Configs as _Configs
+from labml.internal.configs.config_item import ConfigItem
 
 
 class BaseConfigs(_Configs):
@@ -6,3 +9,12 @@ class BaseConfigs(_Configs):
     You should sub-class this class to create your own configurations
     """
     pass
+
+
+def option(config_item: any):
+    assert isinstance(config_item, ConfigItem)
+
+    def wrapper(func: Callable):
+        return config_item(func)
+
+    return wrapper
