f6735297d92d5d9f9355f3ee164d055a75f395a4
Varuna Jayasiri <vpjayasiri@gmail.com>
2020-07-12 11:19:34 +0530

    ✨ run experiments in new processes

--- labml/manage.py
+++ labml/manage.py
@@ -1,15 +1,63 @@
 import importlib
+import multiprocessing as mp
+import multiprocessing.connection
 from pathlib import PurePath
-from typing import Dict
+from typing import Dict, Optional
+
+from labml import lab
 from labml.internal import experiment as _experiment
 
 
-def new_run(python_file: PurePath, configs: Dict[str, any]):
-    from labml import lab
-    lab_path = lab.get_path()
+def new_run(python_file: PurePath, configs: Dict[str, any], lab_path: Optional[PurePath] = None):
+    if lab_path is None:
+        lab_path = lab.get_path()
     module_path = python_file.relative_to(lab_path)
     module_path = str(module_path).replace('/', '.').replace('.py', '')
     _experiment.global_params_singleton().configs = configs
     experiment_module = importlib.import_module(module_path)
 
     experiment_module.main()
+
+
+class Process:
+    _child: mp.connection.Connection
+    _process: mp.Process
+
+    def __init__(self, python_file: PurePath, configs: Dict[str, any], lab_path: PurePath):
+        self._child, parent = mp.Pipe()
+        self._process = mp.Process(target=_run_process,
+                                   args=(parent, python_file, configs, lab_path))
+
+    def __call__(self):
+        self._process.start()
+        ready = self._child.recv()
+        assert ready == 'ready'
+        print(self._process.pid)
+
+        self._child.send("start")
+
+
+def _run_process(remote: multiprocessing.connection.Connection,
+                 python_file: PurePath, configs: Dict[str, any], lab_path: PurePath):
+    module_path = python_file.relative_to(lab_path)
+    module_path = str(module_path).replace('/', '.').replace('.py', '')
+    _experiment.global_params_singleton().configs = configs
+    experiment_module = importlib.import_module(module_path)
+    remote.send("ready")
+
+    while True:
+        try:
+            cmd = remote.recv()
+            if cmd == "start":
+                experiment_module.main()
+            else:
+                raise NotImplementedError
+        except KeyboardInterrupt as e:
+            print("Interrupted")
+            print(e)
+            break
+
+
+def new_run_process(python_file: PurePath, configs: Dict[str, any]):
+    p = Process(python_file, configs, lab.get_path())
+    p()
--- test/manage.py
+++ test/manage.py
@@ -8,5 +8,13 @@ def _test():
                    {'optimizer': 'sgd_optimizer'})
 
 
+def _test_process():
+    manage.new_run_process(PurePath('/home/varuna/ml/lab/test/mnist.py'),
+                           {'optimizer': 'sgd_optimizer'})
+    manage.new_run_process(PurePath('/home/varuna/ml/lab/test/mnist.py'),
+                           {'optimizer': 'adam_optimizer'})
+
+
 if __name__ == '__main__':
-    _test()
+    # _test()
+    _test_process()
