c49a118387828e1549539400d1c9cfa5dcdac5a4
Varuna Jayasiri <vpjayasiri@gmail.com>
2020-11-19 15:42:08 +0530

    🐎 web api repeated configs

--- labml/internal/tracker/writers/web_api.py
+++ labml/internal/tracker/writers/web_api.py
@@ -6,8 +6,9 @@ import time
 import urllib.error
 import urllib.request
 import webbrowser
+from dataclasses import dataclass
 from queue import Queue
-from typing import Dict, Optional
+from typing import Dict, Optional, Callable
 
 import numpy as np
 
@@ -25,28 +26,44 @@ MAX_BUFFER_SIZE = 1024
 WARMUP_COMMITS = 5
 TIMEOUT_SECONDS = 5
 
+_STATIC_ATTRIBUTES = ['configs', 'wildcard_indicators', 'indicators']
+
+
+@dataclass
+class Packet:
+    data: Dict[str, any]
+    callback: Optional[Callable] = None
+    idx: int = -1
+    program_completed: bool = False
+
 
 class WebApiThread(threading.Thread):
     def __init__(self, url: str, verify_connection: bool):
         super().__init__(daemon=False)
         self.verify_connection = verify_connection
         self.url = url
-        self.queue = Queue()
+        self.queue: Queue[Packet] = Queue()
         self.is_stopped = False
         self.warned_updating = False
         self.error = False
+        self.key_idx = {}
+        self.data_idx = 0
 
-    def push(self, data: any):
-        self.queue.put(data)
+    def push(self, packet: Packet):
+        for k in packet.data:
+            self.key_idx[k] = self.data_idx
+        packet.idx = self.data_idx
+        self.queue.put(packet)
+        self.data_idx += 1
 
     def stop(self):
         self.is_stopped = True
-        self.push('done')
+        self.push(Packet(data={}, program_completed=True))
 
     def run(self):
         while True:
-            data = self.queue.get()
-            if data == 'done':
+            packet = self.queue.get()
+            if packet.program_completed:
                 if self.warned_updating:
                     logger.log('Finished updating LabML App.', Text.highlight)
                 return
@@ -54,25 +71,35 @@ class WebApiThread(threading.Thread):
                 if self.is_stopped and not self.warned_updating:
                     logger.log('Still updating LabML App, please wait for it to complete..', Text.highlight)
                     self.warned_updating = True
-                self._process(data)
+                self._process(packet)
 
-    def _process(self, data: Dict[str, any]):
-        res = self._send(data['packet'])
-        if 'accept' in data:
-            data['accept'](res)
+    def _process(self, packet: Packet):
+        data = packet.data
+        remove = [k for k in data if self.key_idx[k] != packet.idx]
+        for k in remove:
+            del data[k]
+
+        if not data:
+            assert packet.callback is None
+            return
+
+        res = self._send(data)
+        if packet.callback is not None:
+            packet.callback(res)
 
     def _send(self, data: Dict[str, any]):
         req = urllib.request.Request(self.url)
         req.add_header('Content-Type', 'application/json; charset=utf-8')
-        data = json.dumps(data)
-        data = data.encode('utf-8')
+        data_json = json.dumps(data)
+        data_json = data_json.encode('utf-8')
         req.add_header('Content-Length', str(len(data)))
 
         try:
+            print('sending', data.keys())
             if self.verify_connection:
-                response = urllib.request.urlopen(req, data, timeout=TIMEOUT_SECONDS)
+                response = urllib.request.urlopen(req, data_json, timeout=TIMEOUT_SECONDS)
             else:
-                response = urllib.request.urlopen(req, data, timeout=TIMEOUT_SECONDS,
+                response = urllib.request.urlopen(req, data_json, timeout=TIMEOUT_SECONDS,
                                                   context=ssl._create_unverified_context())
             content = response.read().decode('utf-8')
             result = json.loads(content)
@@ -153,7 +180,7 @@ class Writer(WriteBase):
         self.name = name
         self.comment = comment
 
-    def push(self, data):
+    def push(self, data: Packet):
         if self.thread is None:
             url = f'{self.web_api.url}run_uuid={self.run_uuid}&labml_version={labml.__version__}'
             self.thread = WebApiThread(url, self.web_api.verify_connection)
@@ -171,9 +198,7 @@ class Writer(WriteBase):
         if self.web_api is None:
             return
 
-        data = {'configs': configs}
-
-        self.push({'packet': data})
+        self.push(Packet({'configs': configs}))
 
     def start(self):
         if self.web_api is None:
@@ -189,7 +214,7 @@ class Writer(WriteBase):
         self.last_committed = time.time()
         self.commits_count = 0
 
-        self.push({'packet': data, 'accept': self._started})
+        self.push(Packet(data, callback=self._started))
 
     def save_indicators(self, dot_indicators: Dict[str, Indicator], indicators: Dict[str, Indicator]):
         if self.web_api is None:
@@ -197,12 +222,8 @@ class Writer(WriteBase):
 
         wildcards = {k: ind.to_dict() for k, ind in dot_indicators.items()}
         inds = {k: ind.to_dict() for k, ind in indicators.items()}
-        data = {
-            'wildcard_indicators': wildcards,
-            'indicators': inds
-        }
 
-        self.push({'packet': data})
+        self.push(Packet({'wildcard_indicators': wildcards, 'indicators': inds}))
 
     def _started(self, url):
         if url is None:
@@ -259,6 +280,7 @@ class Writer(WriteBase):
         self.flush()
 
         # TODO: Will have to fix this when there are other statuses that dont stop the experiment
+        # This will stop the thread after sending all the data
         self.thread.stop()
 
     def flush(self):
@@ -287,8 +309,8 @@ class Writer(WriteBase):
 
         self.indicators = {}
 
-        self.push({'packet': {
+        self.push(Packet({
             'track': data,
             'status': self.state,
             'time': time.time()
-        }})
+        }))
