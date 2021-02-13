d89dba0da3610223d166363bf7c8f993403fccb6
Varuna Jayasiri <vpjayasiri@gmail.com>
2021-02-13 10:25:53 +0530

    ✨ cache set

--- labml/utils/cache.py
+++ labml/utils/cache.py
@@ -39,3 +39,15 @@ def cache_get(name: str, file_type: str = 'json') -> Any:
                 raise ValueError(f'Unknown file type: {file_type}')
     else:
         return None
+
+
+def cache_set(name: str, value: Any, file_type: str = 'json') -> Any:
+    cache_path = lab.get_data_path() / 'cache'
+    if not cache_path.exists():
+        cache_path.mkdir(parents=True)
+    path = cache_path / f'{name}.{file_type}'
+    with open(str(path), 'w') as f:
+        if file_type == 'json':
+            json.dump(value, f)
+        else:
+            raise ValueError(f'Unknown file type: {file_type}')
