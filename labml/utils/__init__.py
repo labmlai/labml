abcd204619adeb38f1a4ad00d80ba6b287cd2b32
Adrien Wu <adrien1018@users.noreply.github.com>
2021-05-10 14:01:14 +0800

    Fix config file discovery on Windows (#62)
    * Fix config file reading on Windows
    
    * Fix crash when running from Jupyter

--- labml/utils/__init__.py
+++ labml/utils/__init__.py
@@ -1,4 +1,4 @@
-import os
+import os, os.path
 from  pathlib import Path
 from typing import Set, List
 
@@ -14,15 +14,15 @@ def get_caller_file(ignore_callers: Set[str] = None) -> str:
 
     for f in frames:
         module_path = Path(f.filename)
-        if str(module_path).startswith(str(lab_src) + '/'):
+        if str(module_path).startswith(os.path.join(lab_src, '')):
             continue
         if str(module_path) in ignore_callers:
             continue
-        if not module_path.exists():
-            break
         if str(module_path).startswith('<stdin'):
             break
         if str(module_path).startswith('<ipython'):
+            break
+        if not module_path.exists():
             break
         if str(module_path.absolute()).find('/dist-packages/') != -1:
             continue
