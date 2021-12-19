4dd3bbcdb7a689818342aff1f2dddaa7dcff969d
hnipun <hnipun@gmail.com>
2021-12-19 15:37:28 +0530

    fixed error in check_labml_token_permission

--- app/server/labml_app/scripts/clean_sessions.py
+++ app/server/labml_app/scripts/clean_sessions.py
@@ -6,5 +6,5 @@ init_db()
 project_keys = project.Project.get_all()
 for project_key in monit.iterate('Projects', project_keys):
     p: project.Project = project_key.load()
-    p.sessions = []
+    p.sessions = {}
     p.save()
