e419ac067802b856532d524b76f2e6a3b4239ef0
hnipun <hnipun@gmail.com>
2021-12-19 15:24:33 +0530

    fixed error in check_labml_token_permission

--- app/server/labml_app/auth/__init__.py
+++ app/server/labml_app/auth/__init__.py
@@ -24,12 +24,15 @@ def get_app_token(request: Request) -> 'app_token.AppToken':
 
 def check_labml_token_permission(func) -> functools.wraps:
     @functools.wraps(func)
-    def wrapper(*args, **kwargs):
+    async def wrapper(*args, **kwargs):
         labml_token = kwargs.get('labml_token', '')
 
         p = project.get_project(labml_token)
         if p and p.is_sharable:
-            return func(*args, **kwargs)
+            if inspect.iscoroutinefunction(func):
+                return await func(*args, **kwargs)
+            else:
+                return func(*args, **kwargs)
 
         kwargs['labml_token'] = None
 
