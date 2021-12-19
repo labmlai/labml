69e75c711172f3f2e0afd13e8f469fbc16ca6eca
hnipun <hnipun@gmail.com>
2021-12-19 19:17:35 +0530

    clean tokens

--- app/server/labml_app/scripts/clean_tokens.py
+++ app/server/labml_app/scripts/clean_tokens.py
@@ -7,6 +7,8 @@ count = 0
 
 token_keys = app_token.AppToken.get_all()
 for token_key in monit.iterate('Tokens', token_keys):
+    if not token_key:
+        continue
     at: app_token.AppToken = token_key.load()
     if not at:
         continue
