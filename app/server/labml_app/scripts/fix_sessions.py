0a3c3298fd97cf89fcd3faa5a84de559f54948b2
hnipun <hnipun@gmail.com>
2021-12-23 22:16:18 +0530

    fix sessions

---
+++ app/server/labml_app/scripts/fix_sessions.py
@@ -0,0 +1,12 @@
+from labml import monit
+from server.labml_app.db import computer, init_db
+
+res = {}
+
+init_db()
+computer_keys = computer.Computer.get_all()
+for computer_key in monit.iterate('computers', computer_keys):
+    c: computer.Computer = computer_key.load()
+
+    res[type(c.sessions)] = res.get(type(c.sessions), 0)
+    res[type(c.sessions)] += 1
