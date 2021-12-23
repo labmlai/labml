98711770394b0bdba9103dc3311f0a5cd422e4d6
hnipun <hnipun@gmail.com>
2021-12-23 22:22:37 +0530

    fix sessions

--- app/server/labml_app/scripts/fix_sessions.py
+++ app/server/labml_app/scripts/fix_sessions.py
@@ -8,6 +8,8 @@ computer_keys = computer.Computer.get_all()
 for computer_key in monit.iterate('computers', computer_keys):
     c: computer.Computer = computer_key.load()
 
-    res[type(c.sessions)] = res.get(type(c.sessions), 0)
-    res[type(c.sessions)] += 1
+    if type(c.sessions) == list:
+        c.sessions = set()
+        c.save()
+
 print(res)
