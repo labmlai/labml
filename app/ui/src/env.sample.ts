95053a29ab76b2866253f6193c6140683adbd8dd
Adithya Narasinghe <adithyasnarasinghe@gmail.com>
2022-08-09 13:24:21 +0530

    Update project to support latest version of weya

--- app/ui/src/env.sample.ts
+++ app/ui/src/env.sample.ts
@@ -3,3 +3,4 @@ export const APP_BASE_URL = `http://localhost:${PORT}`
 export const API_BASE_URL = `http://localhost:${PORT}/api/v1`
 export const MOBILE_APP_NAMESPACE = ''
 export const SENTRY_DSN = ''
+export const DEBUG = false
--- app/ui/src/utils/redirect.ts
+++ app/ui/src/utils/redirect.ts
@@ -5,6 +5,7 @@ import {PageNotFoundHandler} from '../views/errors/page_not_found_view'
 import {AuthErrorHandler} from '../views/errors/auth_error_view'
 import {OtherErrorHandler} from '../views/errors/other_error_view'
 import {NetworkErrorHandler} from '../views/errors/network_error_view'
+import {DEBUG} from '../env'
 
 export function handleNetworkError(error: Error | NetworkError) {
     if (error instanceof NetworkError) {
@@ -23,6 +24,9 @@ export function handleNetworkError(error: Error | NetworkError) {
 }
 
 export function handleNetworkErrorInplace(error: Error | NetworkError) {
+    if(DEBUG){
+        console.error(error)
+    }
     if (error instanceof NetworkError) {
         if (error.statusCode === 404 || error.statusCode === 400) {
             PageNotFoundHandler.handlePageNotFound()
