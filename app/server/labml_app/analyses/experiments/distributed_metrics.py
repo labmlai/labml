5ba923f1cc3373e1f23cd651a794914ddd9084d2
lakshith <lakshith.k.nishshanke@gmail.com>
2023-10-26 09:30:07 +0530

    remove distributed metric tracking endpoint

--- app/server/labml_app/analyses/experiments/distributed_metrics.py
+++ app/server/labml_app/analyses/experiments/distributed_metrics.py
@@ -9,7 +9,6 @@ from labml_db.serializer.yaml import YamlSerializer
 from ..analysis import Analysis
 from . import metrics
 from ..preferences import Preferences
-from ...db import run
 from ...logger import logger
 
 
@@ -54,34 +53,6 @@ async def set_metrics_preferences(request: Request, run_uuid: str) -> Any:
     logger.debug(f'update distributed metrics preferences: {mp.key}')
 
     return {'errors': mp.errors}
-
-
-@Analysis.route('GET', 'distributed/metrics/{run_uuid}')
-async def get_dist_metrics_tracking(request: Request, run_uuid: str) -> Any:
-    track_data = []
-    status_code = 404
-
-    r = run.get(run_uuid)
-
-    run_uuids = []
-    for i in range(r.world_size):
-        if i == 0:
-            run_uuids.append(run_uuid)
-        else:
-            run_uuids.append(f'{run_uuid}_{i}')
-
-    metric_analyses = metrics.mget(run_uuids)
-    for i, ma in enumerate(metric_analyses):
-        if ma:
-            track_data.append(metrics.MetricsAnalysis(ma).get_tracking())
-            status_code = 200
-        else:
-            track_data.append([])
-
-    response = JSONResponse({'series': track_data, 'insights': []})
-    response.status_code = status_code
-
-    return response
 
 
 @Analysis.route('GET', 'distributed/metrics/merged/{run_uuid}')
