8087c1cb34d877c11b279ae74697e1d445898d87
hnipun <hnipun@gmail.com>
2023-11-01 17:04:48 +0530

    no user login

--- app/server/labml_app/auth/__init__.py
+++ app/server/labml_app/auth/__init__.py
@@ -1,22 +1,15 @@
 import functools
-import inspect
 import json
 from typing import Optional
 
 from fastapi import Request
-from fastapi.responses import JSONResponse
 
-from .. import settings
 from ..db import project
 from ..db import user
 from ..db.user import User
 
 
 def get_app_token(request: Request) -> ('str', User):
-    if not settings.IS_LOGIN_REQUIRED:
-        token, u = _login_not_required()
-        return token, u
-
     res = request.headers.get('Authorization', '')
     try:
         res = json.loads(res)
@@ -33,75 +26,7 @@ def get_app_token(request: Request) -> ('str', User):
     return token, u
 
 
-def check_labml_token_permission(func) -> functools.wraps:
-    @functools.wraps(func)
-    async def wrapper(*args, **kwargs):
-        labml_token = kwargs.get('labml_token', '')
-
-        p = project.get_project(labml_token)
-        if p and p.is_sharable:
-            return await func(*args, **kwargs)
-
-        kwargs['labml_token'] = None
-
-        return await func(*args, **kwargs)
-
-    return wrapper
-
-
-# TODO: fix this
-def _login_not_required():
-    u = user.get_or_create_user(identifier='local', is_local_user=True)
-
-    return 'local', u
-
-
-def login_required(func) -> functools.wraps:
-    @functools.wraps(func)
-    async def wrapper(request: Request, *args, **kwargs):
-        token, u = get_app_token(request)
-
-        kwargs['token'] = token
-        if u is None:
-            error_end = 'view this content' if request.method == 'GET' else 'perform this action'
-            response = JSONResponse({'data': {'error': f'You need to be authorized to {error_end}'}, 'meta': {}})
-            response.status_code = 403
-
-            return response
-
-        if inspect.iscoroutinefunction(func):
-            res = await func(request, *args, **kwargs)
-        else:
-            res = func(request, *args, **kwargs)
-
-        if isinstance(res, tuple):
-            res, res_code = res
-        else:
-            res_code = 200 if res else 404
-
-        if res.get('token'):
-            token = res['token']
-
-        response = JSONResponse(res)
-
-        response.headers["Authorization"] = f'{token}'
-        response.status_code = res_code
-
-        return response
-
-    return wrapper
-
-
 def get_auth_user(request: Request) -> Optional['user.User']:
     token, u = get_app_token(request)
 
     return u
-
-
-def get_is_user_logged(request: Request) -> bool:
-    token, u = get_app_token(request)
-
-    if u is None:
-        return False
-
-    return True
--- app/server/labml_app/handlers.py
+++ app/server/labml_app/handlers.py
@@ -9,7 +9,7 @@ from fastapi.responses import JSONResponse
 from .logger import logger
 from . import settings
 from . import auth
-from .db import run, password_reset
+from .db import run
 from .db import computer
 from .db import session
 from .db import user
@@ -18,7 +18,6 @@ from .db import blocked_uuids
 from .db import job
 from . import utils
 from . import analyses
-from .auth.auth_models import SignInModel, SignUpModel
 
 try:
     import requests
@@ -37,82 +36,6 @@ def _is_new_run_added(request: Request) -> bool:
         is_run_added = u.default_project.is_run_added
 
     return is_run_added
-
-
-@utils.analytics.AnalyticsEvent.time_this(None)
-async def sign_in(request: Request, data: SignInModel):
-    authenticated_token = user.authenticate(data.email, data.password)
-    if authenticated_token is None:
-        return JSONResponse({'is_successful': False, 'error': 'The email or password is incorrect'}, status_code=401)
-
-    u = user.get_user_secure(authenticated_token)
-    utils.analytics.AnalyticsEvent.people_set(identifier=u.email, first_name=u.name, last_name='', email=u.email)
-
-    return JSONResponse({'is_successful': True, 'token': authenticated_token}, status_code=200,
-                        headers={'Authorization': authenticated_token})
-
-
-@utils.analytics.AnalyticsEvent.time_this(None)
-async def sign_up(request: Request, data: SignUpModel):
-    u = user.get_or_create_user(utils.gen_token())
-    if not u:
-        return JSONResponse({'is_successful': False, 'error': 'Bots are not allowed to sign up'}, status_code=401)
-    result = u.upgrade_user(data.name, data.email, data.password)
-    if result == -1:
-        return JSONResponse({'is_successful': False, 'error': 'The account is already created'}, status_code=405)
-    elif result == -2:
-        return JSONResponse({'is_successful': False, 'error': 'The email is already in use'}, status_code=422)
-    elif result == -4:
-        return JSONResponse({'is_successful': False, 'error': 'The handle is already in use'}, status_code=422)
-    elif result != 0:
-        return JSONResponse({'is_successful': False, 'error': UNKNOWN_ERROR_MESSAGE}, status_code=500)
-
-    authenticated_token = user.authenticate(data.email, data.password)
-    utils.analytics.AnalyticsEvent.people_set(identifier=u.email, first_name=u.name, last_name='', email=u.email)
-
-    return JSONResponse({'is_successful': True, 'token': authenticated_token}, status_code=200,
-                        headers={'Authorization': authenticated_token})
-
-
-@utils.analytics.AnalyticsEvent.time_this(None)
-async def reset_password(request: Request):
-    json = await request.json()
-    if 'reset_token' not in json or 'new_password' not in json:
-        return JSONResponse({'is_successful': False, 'error': 'Malformed request'}, status_code=400)
-
-    reset_token = json['reset_token']
-    new_password = json['new_password']
-
-    rp = password_reset.get(reset_token)
-    if not rp:
-        return JSONResponse({'is_successful': False,
-                             'error': 'The reset token is either invalid or expired. Please request a new password '
-                                      'reset link.'},
-                            status_code=401)
-    if not rp.is_valid:
-        password_reset.delete_token(rp)
-        return JSONResponse({'is_successful': False,
-                             'error': 'The reset token is either invalid or expired. Please request a new password '
-                                      'reset link.'},
-                            status_code=401)
-
-    # should not happen
-    if rp.reset_token != reset_token:
-        return JSONResponse({'is_successful': False,
-                             'error': 'Internal Error. If the problem persist, please reach us out at contact@labml.ai'},
-                            status_code=500)
-
-    u = rp.user.load()
-    u.reset_password(new_password)
-    password_reset.delete_token(rp)
-
-    return JSONResponse({'is_successful': True})
-
-
-@auth.login_required
-@utils.analytics.AnalyticsEvent.time_this(None)
-async def sign_out(request: Request, token: Optional[str] = None) -> EndPointRes:
-    return {'is_successful': user.invalidate_token(token), 'token': ''}
 
 
 @utils.analytics.AnalyticsEvent.time_this(0.4)
@@ -290,7 +213,6 @@ async def update_session(request: Request) -> EndPointRes:
     return res
 
 
-@auth.login_required
 @utils.analytics.AnalyticsEvent.time_this(None)
 async def claim_run(request: Request, run_uuid: str, token: Optional[str] = None) -> EndPointRes:
     r = run.get(run_uuid)
@@ -315,7 +237,6 @@ async def claim_run(request: Request, run_uuid: str, token: Optional[str] = None
     return {'is_successful': True}
 
 
-@auth.login_required
 @utils.analytics.AnalyticsEvent.time_this(None)
 async def claim_session(request: Request, session_uuid: str, token: Optional[str] = None) -> EndPointRes:
     c = session.get(session_uuid)
@@ -374,7 +295,6 @@ async def get_session(request: Request, session_uuid: str) -> JSONResponse:
     return response
 
 
-@auth.login_required
 async def edit_run(request: Request, run_uuid: str, token: Optional[str] = None) -> EndPointRes:
     r = run.get(run_uuid)
     errors = []
@@ -436,9 +356,7 @@ async def get_session_status(request: Request, session_uuid: str) -> JSONRespons
     return response
 
 
-@auth.login_required
 @utils.analytics.AnalyticsEvent.time_this(None)
-@auth.check_labml_token_permission
 async def get_runs(request: Request, labml_token: str, token: Optional[str] = None) -> EndPointRes:
     u = user.get_by_session_token(token)
 
@@ -460,9 +378,7 @@ async def get_runs(request: Request, labml_token: str, token: Optional[str] = No
     return {'runs': res, 'labml_token': labml_token}
 
 
-@auth.login_required
 @utils.analytics.AnalyticsEvent.time_this(None)
-@auth.check_labml_token_permission
 async def get_sessions(request: Request, labml_token: str, token: Optional[str] = None) -> EndPointRes:
     u = user.get_by_session_token(token)
 
@@ -485,7 +401,6 @@ async def get_sessions(request: Request, labml_token: str, token: Optional[str] 
 
 
 @utils.analytics.AnalyticsEvent.time_this(None)
-@auth.login_required
 async def delete_runs(request: Request, token: Optional[str] = None) -> EndPointRes:
     json = await request.json()
     run_uuids = json['run_uuids']
@@ -497,7 +412,6 @@ async def delete_runs(request: Request, token: Optional[str] = None) -> EndPoint
 
 
 @utils.analytics.AnalyticsEvent.time_this(None)
-@auth.login_required
 async def delete_sessions(request: Request, token: Optional[str] = None) -> EndPointRes:
     json = await request.json()
     session_uuids = json['session_uuids']
@@ -508,7 +422,6 @@ async def delete_sessions(request: Request, token: Optional[str] = None) -> EndP
     return {'is_successful': True}
 
 
-@auth.login_required
 async def add_run(request: Request, run_uuid: str, token: Optional[str] = None) -> EndPointRes:
     u = user.get_by_session_token(token)
 
@@ -517,7 +430,6 @@ async def add_run(request: Request, run_uuid: str, token: Optional[str] = None) 
     return {'is_successful': True}
 
 
-@auth.login_required
 async def add_session(request: Request, session_uuid: str, token: Optional[str] = None) -> EndPointRes:
     u = user.get_by_session_token(token)
 
@@ -526,7 +438,6 @@ async def add_session(request: Request, session_uuid: str, token: Optional[str] 
     return {'is_successful': True}
 
 
-@auth.login_required
 @utils.analytics.AnalyticsEvent.time_this(None)
 async def get_computer(request: Request, computer_uuid: str) -> EndPointRes:
     c = computer.get_or_create(computer_uuid)
@@ -534,7 +445,6 @@ async def get_computer(request: Request, computer_uuid: str) -> EndPointRes:
     return c.get_data()
 
 
-@auth.login_required
 @utils.analytics.AnalyticsEvent.time_this(None)
 async def set_user(request: Request, token: Optional[str] = None) -> EndPointRes:
     u = auth.get_auth_user(request)
@@ -561,124 +471,6 @@ async def get_user(request: Request):
                         headers={'Authorization': session_token})
 
 
-@utils.analytics.AnalyticsEvent.time_this(None)
-async def is_user_logged(request: Request) -> EndPointRes:
-    return {'is_user_logged': auth.get_is_user_logged(request)}
-
-
-@utils.analytics.AnalyticsEvent.time_this(None)
-async def sync_computer(request: Request) -> EndPointRes:
-    """End point to sync UI-server and UI-computer. runs: to sync with the server.
-        """
-    errors = []
-
-    computer_uuid = request.query_params.get('computer_uuid', '')
-    if len(computer_uuid) < 10:
-        error = {'error': 'invalid_computer_uuid',
-                 'message': f'Invalid Computer UUID'}
-        errors.append(error)
-        return {'errors': errors}
-
-    c = computer.get_or_create(computer_uuid)
-
-    json = await request.json()
-    runs = json.get('runs', [])
-    res = c.sync_runs(runs)
-
-    return {'runs': res}
-
-
-@utils.analytics.AnalyticsEvent.time_this(60.4)
-async def polling(request: Request) -> EndPointRes:
-    """End point to sync UI-server and UI-computer. jobs: statuses of jobs.
-    pending jobs will be returned in the response if there any
-           """
-    errors = []
-
-    computer_uuid = request.query_params.get('computer_uuid', '')
-    if len(computer_uuid) < 10:
-        error = {'error': 'invalid_computer_uuid',
-                 'message': f'Invalid Computer UUID'}
-        errors.append(error)
-        return {'errors': errors}
-
-    c = computer.get_or_create(computer_uuid)
-
-    c.update_last_online()
-
-    json = await request.json()
-    job_responses = json.get('jobs', [])
-    if job_responses:
-        c.sync_jobs(job_responses)
-
-    pending_jobs = []
-    for i in range(16):
-        c = computer.get_or_create(computer_uuid)
-        pending_jobs = c.get_pending_jobs()
-        if pending_jobs:
-            break
-
-        await asyncio.sleep(3)
-
-    return {'jobs': pending_jobs}
-
-
-@auth.login_required
-@utils.analytics.AnalyticsEvent.time_this(30.4)
-async def start_tensor_board(request: Request, computer_uuid: str, token: Optional[str] = None) -> EndPointRes:
-    """End point to start TB for set of runs. runs: all the runs should be from a same computer.
-            """
-    c = computer.get_or_create(computer_uuid)
-
-    if not c.is_online:
-        return {'status': job.JobStatuses.COMPUTER_OFFLINE, 'data': {}}
-
-    json = await request.json()
-    runs = json.get('runs', [])
-    j = c.create_job(job.JobMethods.START_TENSORBOARD, {'runs': runs})
-
-    for i in range(15):
-        c = computer.get_or_create(computer_uuid)
-        completed_job = c.get_completed_job(j.job_uuid)
-        if completed_job and completed_job.is_completed:
-            return completed_job.to_data()
-
-        await asyncio.sleep(2)
-
-    data = j.to_data()
-    data['status'] = job.JobStatuses.TIMEOUT
-
-    return data
-
-
-@auth.login_required
-@utils.analytics.AnalyticsEvent.time_this(30.4)
-async def clear_checkpoints(request: Request, computer_uuid: str, token: Optional[str] = None) -> EndPointRes:
-    """End point to clear checkpoints for set of runs. runs: all the runs should be from a same computer.
-            """
-    c = computer.get_or_create(computer_uuid)
-
-    if not c.is_online:
-        return {'status': job.JobStatuses.COMPUTER_OFFLINE, 'data': {}}
-
-    json = await request.json()
-    runs = json.get('runs', [])
-    j = c.create_job(job.JobMethods.CLEAR_CHECKPOINTS, {'runs': runs})
-
-    for i in range(15):
-        c = computer.get_or_create(computer_uuid)
-        completed_job = c.get_completed_job(j.job_uuid)
-        if completed_job and completed_job.is_completed:
-            return completed_job.to_data()
-
-        await asyncio.sleep(2)
-
-    data = j.to_data()
-    data['status'] = job.JobStatuses.TIMEOUT
-
-    return data
-
-
 def _add_server(app: FastAPI, method: str, func: Callable, url: str):
     if not inspect.iscoroutinefunction(func):
         raise ValueError(f'{func.__name__} is not a async function')
@@ -696,8 +488,6 @@ def _add_ui(app: FastAPI, method: str, func: Callable, url: str):
 def add_handlers(app: FastAPI):
     _add_server(app, 'POST', update_run, 'track')
     _add_server(app, 'POST', update_session, 'computer')
-    _add_server(app, 'POST', sync_computer, 'sync')
-    _add_server(app, 'POST', polling, 'polling')
 
     _add_ui(app, 'GET', get_runs, 'runs/{labml_token}')
     _add_ui(app, 'PUT', delete_runs, 'runs')
@@ -720,17 +510,9 @@ def add_handlers(app: FastAPI):
 
     _add_ui(app, 'POST', set_user, 'user')
     _add_ui(app, 'POST', get_user, 'auth/user')
-    _add_ui(app, 'POST', sign_in, 'auth/sign_in')
-    _add_ui(app, 'POST', sign_up, 'auth/sign_up')
-    _add_ui(app, 'POST', sign_out, 'auth/sign_out')
-    _add_ui(app, 'POST', reset_password, 'auth/password_reset')
-    _add_ui(app, 'GET', is_user_logged, 'auth/is_logged')
-
-    _add_ui(app, 'POST', start_tensor_board, 'start_tensorboard/{computer_uuid}')
-    _add_ui(app, 'POST', clear_checkpoints, 'clear_checkpoints/{computer_uuid}')
 
     for method, func, url, login_required in analyses.AnalysisManager.get_handlers():
         if login_required:
-            func = auth.login_required(func)
+            func = func
 
         _add_ui(app, method, func, url)
