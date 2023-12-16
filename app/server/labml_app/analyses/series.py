db4ad58c5def2915bd1a87e2eebae808dd90fd9c
lakshith <lakshith.k.nishshanke@gmail.com>
2023-12-16 13:17:15 +0530

    correctly trim last step

--- app/server/labml_app/analyses/series.py
+++ app/server/labml_app/analyses/series.py
@@ -12,7 +12,7 @@ OUTLIER_MARGIN = 0.04
 SeriesModel = Dict[str, Union[np.ndarray, List[float], float]]
 
 
-def _remove_old(values, steps):
+def _remove_old(values, steps, last_steps):
     break_time = datetime.fromtimestamp(steps[-1]) - timedelta(days=1)
 
     left = 0
@@ -30,7 +30,7 @@ def _remove_old(values, steps):
     # find index to break
     break_index = left
 
-    return values[break_index:], steps[break_index:]
+    return values[break_index:], steps[break_index:], last_steps[break_index]
 
 
 class Series:
@@ -121,8 +121,7 @@ class Series:
         self.last_step = np.concatenate((self.last_step, last_step))
 
         if self.keep_last_24h:
-            self.value, self.step = _remove_old(self.value, self.step)
-            self.last_step = self.step.copy()
+            self.value, self.step, self.last_step = _remove_old(self.value, self.step, self.last_step)
 
         self.step_gap = self.find_step_gap()
 
