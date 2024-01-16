edeee59f4ebb6bb335ac5f02349554f2fd487676
hnipun <hnipun@gmail.com>
2024-01-16 15:56:03 +0530

    no more analytics

--- app/server/unit_tests/get_run.py
+++ app/server/unit_tests/get_run.py
@@ -3,10 +3,12 @@ from labml_app.settings import FLOAT_PROJECT_TOKEN
 
 init_mongo_db()
 
-r = run.get('5372ad8ab45311eeb035946dae1e1468')
+r = run.get('c')
 
 print(r.run_uuid)
 print(r.is_claimed)
 
 float_project = project.get_project(labml_token=FLOAT_PROJECT_TOKEN)
 print(r.run_uuid in float_project.runs)
+
+print(project.get_run('5372ad8ab45311eeb035946dae1e1468', FLOAT_PROJECT_TOKEN).run_uuid)
