4f7df38e2c740ffba1be63d87d51900aff4b78e1
hnipun <hnipun@gmail.com>
2024-01-16 15:36:14 +0530

    no more analytics

--- app/server/unit_tests/get_run.py
+++ app/server/unit_tests/get_run.py
@@ -1,4 +1,5 @@
-from labml_app.db import run, init_mongo_db
+from labml_app.db import run, init_mongo_db, project
+from labml_app.settings import FLOAT_PROJECT_TOKEN
 
 init_mongo_db()
 
@@ -6,3 +7,6 @@ r = run.get('5372ad8ab45311eeb035946dae1e1468')
 
 print(r.run_uuid)
 print(r.is_claimed)
+
+float_project = project.get_project(labml_token=FLOAT_PROJECT_TOKEN)
+print(r.run_uuid in float_project.runs)
