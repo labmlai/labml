dd152377a89df7c8ddc900798b068446d5f83c34
hnipun <hnipun@gmail.com>
2024-03-17 19:32:38 +0000

    fixed escaped chars

--- app/server/unit_tests/get_run.py
+++ app/server/unit_tests/get_run.py
@@ -1,14 +1,16 @@
-from labml_app.db import run, init_mongo_db, project
+from labml_app.db import run, init_mongo_db, status
 from labml_app.settings import FLOAT_PROJECT_TOKEN
 
 init_mongo_db()
+run_uuid = 'b07f401ee44c11ee8a78c4cbe1b5376c'
 
-r = run.get('c')
+r = run.get('b07f401ee44c11ee8a78c4cbe1b5376c')
+print(r)
 
-print(r.run_uuid)
-print(r.is_claimed)
+r = run.get(run_uuid)
+if r is not None:
+    uuids = [f'{run_uuid}_{i}' for i in range(1, r.world_size)]
+    uuids.append(run_uuid)
+    status_data = run.get_merged_status_data(uuids)
 
-float_project = project.get_project(labml_token=FLOAT_PROJECT_TOKEN)
-print(r.run_uuid in float_project.runs)
-
-print(project.get_run('5372ad8ab45311eeb035946dae1e1468', FLOAT_PROJECT_TOKEN).run_uuid)
+print(status_data)
