b67fb7dc5d613262ed996495da63d689f11bf96b
lakshith <lakshith.k.nishshanke@gmail.com>
2024-05-10 19:02:05 +0530

    use step gaps for smoothins

--- app/ui/src/components/charts/smoothing/two_sided_exponential_average.ts
+++ app/ui/src/components/charts/smoothing/two_sided_exponential_average.ts
@@ -21,37 +21,38 @@ export class TwoSidedExponentialAverage extends SeriesSmoothing {
 
             let last_sum = 0
             let last_weight = 0
+            let last_step = ind.series[0].step
             for (let j = 0; j < ind.series.length; j++) {
-                f_sum.push(last_sum * smoothingFactor + ind.series[j].value)
-                f_weights.push(last_weight * smoothingFactor + 1)
+                let smooth_gap = Math.pow(smoothingFactor, Math.abs(ind.series[j].step - last_step))
+                f_sum.push(last_sum * smooth_gap + ind.series[j].value)
+                f_weights.push(last_weight * smooth_gap + 1)
 
                 last_sum = f_sum[j]
                 last_weight = f_weights[j]
+                last_step = ind.series[j].step
             }
 
             last_sum = 0
             last_weight = 0
+            last_step = ind.series[ind.series.length - 1].step
             for (let j = ind.series.length - 1; j >= 0; j--) {
-                b_sum.push(last_sum * smoothingFactor + ind.series[j].value)
-                b_weights.push(last_weight * smoothingFactor + 1)
+                let smooth_gap = Math.pow(smoothingFactor, Math.abs(ind.series[j].step - last_step))
+                b_sum.push(last_sum * smooth_gap + ind.series[j].value)
+                b_weights.push(last_weight * smooth_gap + 1)
 
                 last_sum = b_sum[b_sum.length - 1]
                 last_weight = b_weights[b_weights.length - 1]
+                last_step = ind.series[j].step
             }
             b_weights.reverse()
             b_sum.reverse()
 
-            for (let j = 0; j < ind.series.length-1; j++) {
-                let smoothed = (f_sum[j] + smoothingFactor * b_sum[j+1]) /
-                    (f_weights[j] + smoothingFactor * b_weights[j+1])
+            for (let j = 0; j < ind.series.length; j++) {
+                let smoothed = (f_sum[j] + b_sum[j] - ind.series[j].value) /
+                    (f_weights[j] + b_weights[j] - 1)
                 result.push({step: ind.series[j].step, value: ind.series[j].value,
                     smoothed: smoothed, lastStep: ind.series[j].lastStep})
             }
-            result.push({step: ind.series[ind.series.length-1].step, value: ind.series[ind.series.length-1].value,
-                smoothed: f_sum[ind.series.length-1] / f_weights[ind.series.length-1],
-                lastStep: ind.series[ind.series.length-1].lastStep})
-
-
 
             ind.series = result
         }
