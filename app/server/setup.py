fa4b6e1f65e61f6f718f64bcbd28222e30806e24
Varuna Jayasiri <vpjayasiri@gmail.com>
2024-07-08 15:54:11 +0530

    version

--- app/server/setup.py
+++ app/server/setup.py
@@ -5,7 +5,7 @@ with open("../readme.md", "r") as f:
 
 setuptools.setup(
     name='labml_app',
-    version='0.0.102',
+    version='0.0.103',
     author="Varuna Jayasiri, Nipun, Aditya",
     author_email="vpjayasiri@gmail.com",
     description="",
