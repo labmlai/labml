8dd643da4598caed1693a6fca959d40203412a29
hnipun <hnipun@gmail.com>
2024-10-18 13:26:20 +0530

    loader

--- app/server/labml_app/scripts/check_run.py
+++ app/server/labml_app/scripts/check_run.py
@@ -13,6 +13,9 @@ uuid = 'c48ac8bc8ccb11efb77ba088c26a9b7a_1'
 for i in range(1, 7):
     print(f'c48ac8bc8ccb11efb77ba088c26a9b7a_{i}')
     r = run.get(f'c48ac8bc8ccb11efb77ba088c26a9b7a_{i}')
-    s = r.status.load().run_status.load()
+    s = r.status.load()
     datetime_object = datetime.fromtimestamp(s.time)
+    print(s.status, datetime_object)
+    rs = s.run_status.load()
+    datetime_object = datetime.fromtimestamp(rs.time)
     print(s.status, datetime_object)
