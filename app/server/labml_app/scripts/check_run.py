037f6fe5fa6ddfb0720ca1a40f31ffac4c0194f3
hnipun <hnipun@gmail.com>
2024-10-18 13:18:27 +0530

    loader

--- app/server/labml_app/scripts/check_run.py
+++ app/server/labml_app/scripts/check_run.py
@@ -3,10 +3,10 @@ from labml_app.db import init_mongo_db, run
 init_mongo_db()
 
 uuid = 'c48ac8bc8ccb11efb77ba088c26a9b7a_1'
-uuid2 = 'c48ac8bc8ccb11efb77ba088c26a9b7a_0'
-r = run.get(uuid2)
-print(r.status.load().run_status.load())
-for i in range(8):
+# uuid2 = 'c48ac8bc8ccb11efb77ba088c26a9b7a_0'
+# r = run.get(uuid2)
+# print(r.status.load().run_status.load())
+for i in range(1, 7):
     print(f'c48ac8bc8ccb11efb77ba088c26a9b7a_{i}')
     r = run.get(f'c48ac8bc8ccb11efb77ba088c26a9b7a_{i}')
     print(r.status.load().run_status.load())
