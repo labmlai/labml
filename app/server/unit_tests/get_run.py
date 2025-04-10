40865faa32c360916112241653285a41ee43cd4f
hnipun <hnipun@gmail.com>
2025-04-10 14:58:57 +0530

    use the port

--- app/server/unit_tests/get_run.py
+++ app/server/unit_tests/get_run.py
@@ -1,8 +1,9 @@
 from labml_app.db import run, init_mongo_db, status
+from labml_app.db import dist_run
 from labml_app.settings import FLOAT_PROJECT_TOKEN
 
 init_mongo_db()
 run_uuid = 'a9fff63415e711f094de00001047fe80'
 
-r = run.get(run_uuid)
+r = dist_run.get_main_run(run_uuid)
 print(r)
