dd2b777476712e573b6da92472769c0f98e4f915
hnipun <hnipun@gmail.com>
2025-04-10 14:53:45 +0530

    use the port

--- app/server/unit_tests/get_run.py
+++ app/server/unit_tests/get_run.py
@@ -2,15 +2,7 @@ from labml_app.db import run, init_mongo_db, status
 from labml_app.settings import FLOAT_PROJECT_TOKEN
 
 init_mongo_db()
-run_uuid = 'b07f401ee44c11ee8a78c4cbe1b5376c'
+run_uuid = 'a9fff63415e711f094de00001047fe80'
 
 r = run.get('b07f401ee44c11ee8a78c4cbe1b5376c')
 print(r)
-
-r = run.get(run_uuid)
-if r is not None:
-    uuids = [f'{run_uuid}_{i}' for i in range(1, r.world_size)]
-    uuids.append(run_uuid)
-    status_data = run.get_merged_status_data(uuids)
-
-print(status_data)
